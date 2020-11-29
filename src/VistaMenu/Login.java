e19bab4367919b7a31cc5412e7e07b0c5d57159c
mabg <mabg@LAPTOP-TUIEGDGS>
2020-11-29 00:54:47 -0600

    Modificacion en el BOton de ingresar del Jframe Login

--- src/VistaMenu/Login.java
+++ src/VistaMenu/Login.java
@@ -14,10 +14,10 @@ import javax.swing.JOptionPane;
  * @author mabg
  */
 public class Login extends javax.swing.JFrame {
-        Connection con;
-	Statement pst;
-	ResultSet rs;
-
+        public static String user;
+        public static String pass;
+        
+ 
  
     public Login() {
         initComponents();
@@ -60,23 +60,24 @@ public class Login extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(btnI)
-                .addGap(50, 50, 50))
             .addGroup(layout.createSequentialGroup()
-                .addGap(163, 163, 163)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel1)
-                    .addComponent(txtC, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
-                    .addComponent(txtU))
-                .addContainerGap(137, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(143, 143, 143)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel1)
+                            .addComponent(txtC)
+                            .addComponent(txtU, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(154, 154, 154)
+                        .addComponent(btnI)))
+                .addContainerGap(157, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(128, 128, 128)
+                .addContainerGap(120, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addGap(7, 7, 7)
                 .addComponent(txtU, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -84,45 +85,35 @@ public class Login extends javax.swing.JFrame {
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(txtC, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
+                .addGap(28, 28, 28)
                 .addComponent(btnI)
-                .addGap(33, 33, 33))
+                .addGap(37, 37, 37))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    
     private void btnIActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIActionPerformed
         // TODO add your handling code here:
+        conexion.ConexionBD.getIngreso(txtU.getText(),String.valueOf(txtC.getPassword()));
+        conexion.ConexionBD.getConnection();
+        boolean s = conexion.ConexionBD.getSt();
+        if (s) {
+            Menu m = new Menu();
+            m.setVisible(true);
+            
+        }else {
+            JOptionPane.showMessageDialog(null,"Usuario y Contraseña Incorrectos","Error De conexion",JOptionPane.ERROR_MESSAGE);
+        }
+        
          
 			
     }//GEN-LAST:event_btnIActionPerformed
 
     private void btnIMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnIMouseClicked
         
-        String user = Login.txtU.getText();
-        String pasw=String.valueOf(txtC.getPassword());
-        int resultado=0;
-       
         
-        try {
-            con= (Connection) ConexionBD.getConnection();
-            pst = con.createStatement("SELECT * FROM usuario WHERE user=? and pass=?");
-            pst.setString(1, user);
-            pst.setString(2, pasw);
-            rs = pst.executeQuery();
-            if (rs.next()) {
-                Menu gui = new Menu();
-                gui.setVisible(true);
-                setVisible(false);
-                }else
-		JOptionPane.showMessageDialog(getParent(),"Contraseña y Usuario Incorrectos Intenta de nuevo");
-					
-        } catch (Exception e) {
-        }
-
-        }
-    
     }//GEN-LAST:event_btnIMouseClicked
 
     /**
